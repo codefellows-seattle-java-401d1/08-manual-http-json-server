6da079296a32f77dcfdb725fd8ee65a702674126
Paul Suarez <paulsblogging.com@gmail.com>
2018-07-19 12:43:31 -0700

    going to branch

---
+++ src/HTTPRequest.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class HTTPRequest {
+    public String path;
+
+    public void HTTPRequest() {
+
+    }
+
+    String request = line.readLine();
+    this.path = line.split(" ")[1];
+
+}
