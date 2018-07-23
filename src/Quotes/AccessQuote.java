73ed1637a38f5571308afaaeb67a4253e8567429
Suzanne Richman <suzanne@favoredfortune.com>
2018-07-22 21:54:12 -0700

    rewatched lecture and built full server and JSON reader. Fixed redirect issues. Despite passing tests, can't get random quote to print - think it has to do with how I'm invoking the method and/or the method's return value, but can't find it. Moving on

--- src/Quotes/AccessQuote.java
+++ src/Quotes/AccessQuote.java
@@ -1,30 +1,31 @@
 package Quotes;
 
 import com.google.gson.Gson;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+
 import java.io.*;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+
 
 public class AccessQuote {
-
-    public static void main(String[] args) {
-        Gson gson = new Gson();
-
+    public static String generateQuote(){
+        Quote randomQuote;
         try {
-            String filepath = "recentquotes.json";
+            Gson gson = new Gson();
+            String filepath = "/Users/sooz/codefellows/401Java/Labs/08-manual-http-json-server/mywebsite/recentquotes.json";
             File file = new File(filepath);
             FileReader reader = new FileReader(file);
 
-            Quote [] quotes = gson. fromJson(reader, Quote[].class);
+            Quote[] quotes = gson. fromJson(reader, Quote[].class);
             System.out.println("Num quotes: " + quotes.length);
+
+
             int randomIndex = (int)Math.floor(quotes.length * (Math.random()));
-            System.out.println(quotes[randomIndex]);
+            randomQuote = quotes[randomIndex];
+            System.out.println(randomQuote);
+            return String.valueOf(randomQuote);
         }
         catch (FileNotFoundException e){
             e.printStackTrace();
         }
+        return "There you go!";
     }
 }
---
+++ src/SimpleManualHTTPServer/HTTPStaticFileReader.java
@@ -0,0 +1,76 @@
+package SimpleManualHTTPServer;
+
+import Quotes.AccessQuote;
+import Quotes.Quote;
+import com.google.gson.stream.JsonReader;
+
+import java.io.IOException;
+import java.io.*;
+import java.net.*;
+import java.util.Date;
+import java.util.Scanner;
+
+//needs access to HTTP Request object and use it to find the path of the requested file, read the file content and
+// then format a HTTP response with the file contents
+public class HTTPStaticFileReader {
+    private static final String ROOT_PATH_PREFIX =
+            "/Users/sooz/codefellows/401Java/Labs/08-manual-http-json-server/mywebsite/";
+    private  String path;
+
+    public HTTPStaticFileReader(HTTPRequest request){
+        this.path = request.path;
+    }
+
+    public String getContents() throws IOException{
+        String result = "";
+
+        String filepath = ROOT_PATH_PREFIX + this.path;
+        File file = new File(filepath);
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            result += processLine(line);
+        }
+        return result;
+    }
+
+    //accepts a line and either returns the plain line, or
+    //detects the template syntax {{SYMBOL_MARKER}} and replaces that portion
+    //of the line with corresponding content for "SYMBOL_MARKER"
+    private String processLine(String line) {
+        if (!line.contains("{{")) {
+            return line;
+        }
+        //"<p>{{RANDOM_JSON_QUOTE}}<p>"
+        // first "<p>"
+        //rest "RANDOM_JSON_QUOTE</p>"
+        String[] cells = line.split("\\{\\{");
+        String first = cells[0];
+        String rest = cells[1];
+
+        cells = rest.split("}}");
+        String symbol = cells [0];
+        String last = cells [1];
+
+        String content ="___";
+
+        if(symbol.equals("RANDOM_JASON_QUOTE")){
+            content = AccessQuote.generateQuote();
+        } else if(symbol.equals("TIMESTAMP")){
+            content = currentTimestamp();
+        } else{
+            content = randomJSONQuote();
+        }
+        return first + content + last;
+    }
+
+    public String randomJSONQuote(){
+        return "\"Our prime purpose in this life is to help others. And if you can't help them, at least don't hurt " +
+                "them.\n - 14th Dalai Lama";
+    }
+
+    public String currentTimestamp(){
+        Date date = new Date();
+        return  date.toString();
+    }
+}
