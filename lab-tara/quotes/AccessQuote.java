9cb844405aad7ab028d78e8589cd7fb1772b2012
Tara Johnson <tara@madface.com>
2018-07-29 16:19:27 -0700

    Quotes displaying on index.html

--- lab-tara/quotes/AccessQuote.java
+++ lab-tara/quotes/AccessQuote.java
@@ -5,11 +5,10 @@ import com.google.gson.Gson;
 import java.io.*;
 
 public class AccessQuote {
-    public static void main(String[] args) {
+    public static Quote renderQuote() {
         try {
             Gson gson = new Gson();
-            String filepath = "C:\\Users\\tbeth\\iCloudDrive\\Desktop\\code-fellows\\java-401d1\\labs\\08-manual-http" +
-                    "-json-server\\lab-tara\\recentquotes.json";
+            String filepath = "C:\\Users\\tbeth\\iCloudDrive\\Desktop\\code-fellows\\java-401d1\\labs\\08-manual-http-json-server\\recentquotes.json";
             File file = new File(filepath);
             FileReader reader = new FileReader(file);
 
@@ -20,9 +19,11 @@ public class AccessQuote {
             int randomIndex = (int) Math.floor(quotes.length * Math.random());
             Quote quote = quotes[randomIndex];
             System.out.println(quote);
+            return quote;
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        return renderQuote();
     }
 }
